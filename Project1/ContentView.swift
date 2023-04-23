a1adaaba2976a0d970dfdd18db6846ba61846482
Jose A Pino <japo@grupoicarus.com.mx>
2023-04-22 23:13:32 -0600

    Update ContentView.swift

--- Project1/ContentView.swift
+++ Project1/ContentView.swift
@@ -7,15 +7,42 @@
 
 import SwiftUI
 
+struct MessageInfoPosgrade: Identifiable {
+    var id: String { name }
+    let name: String
+}
+
 struct ContentView: View {
+    @State private var selectedShow: MessageInfoPosgrade?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Text("Tendencias de Movilidad y Colaboración")
+                .font(.title)
+                .multilineTextAlignment(.leading)
+            
+            HStack {
+                Text("MAESTRÍA EN TECNOLOGÍAS DE LA INFORMACIÓN Y LA COMUNICACIÓN")
+                    .font(.subheadline)
+                    .multilineTextAlignment(.leading)
+                    .padding(.horizontal, 10.0)
+            }
+            Text("Mostrar información de personas del posgrado")
+                .font(.headline)
+                .multilineTextAlignment(.center)
+                .padding(10)
+            
+            Button("Mostrar Docente") {
+                selectedShow = MessageInfoPosgrade(name: "Mtra. Karla Idalia Rosales González")
+            }
+            
+            Button("Mostrar Alumno") {
+                selectedShow = MessageInfoPosgrade(name: "Ing. José A Pino Ocampo")
+            }
         }
-        .padding()
+        .alert(item: $selectedShow) { show in
+            Alert(title: Text(show.name), dismissButton: .cancel())
+        }
     }
 }
 
