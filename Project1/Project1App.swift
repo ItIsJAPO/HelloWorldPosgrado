dfd3148ce1f33217f28632fb0fd56a52ced01785
Jose A Pino <japo@grupoicarus.com.mx>
2023-04-22 22:34:51 -0600

    Initial Commit

---
+++ Project1/Project1App.swift
@@ -0,0 +1,17 @@
+//
+//  Project1App.swift
+//  Project1
+//
+//  Created by JOSE ANTONIO PINO OCAMPO on 22/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
